171ee269737d36f4803ba507763fe6805ff4f995
Deshy Dan <deshy733@gmail.com>
2024-03-09 19:42:02 +0200

    Add file uploader

---
+++ FileUploader/FileUploader/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FileUploader
+//
+//  Created by Gabriel Theodoropoulos.
+//  Copyright © 2019 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let rest = RestManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
