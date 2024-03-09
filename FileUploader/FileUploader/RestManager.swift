8061ec9625a7481abd64df0e279cce93b59246e1
Deshy Dan <deshy733@gmail.com>
2024-03-09 20:10:04 +0200

    Actually don't need this haha

--- FileUploader/FileUploader/RestManager.swift
+++ FileUploader/FileUploader/RestManager.swift
@@ -211,12 +211,134 @@ extension RestManager.CustomError: LocalizedError {
 // MARK: - File Upload Related Implementation
 
 extension RestManager {
+    struct FileInfo{
+        var fileContents: Data?
+        var mimetype:String?
+        var filename:String?
+        var name: String?
+        
+        init(withFileURL url: URL?, filename: String, name: String, mimetype: String) {
+                guard let url = url else { return }
+                fileContents = try? Data(contentsOf: url)
+                self.filename = filename
+                self.name = name
+                self.mimetype = mimetype
+            }}
     
+    private func createBoundary() -> String? {
+        var uuid = UUID().uuidString
+        uuid = uuid.replacingOccurrences(of: "-", with: "")
+        uuid = uuid.map { $0.lowercased() }.joined()
+     
+        let boundary = String(repeating: "-", count: 20) + uuid + "\(Int(Date.timeIntervalSinceReferenceDate))"
+     
+        return boundary
+    }
+    private func getHttpBody(withBoundary boundary: String) -> Data {
+        var body = Data()
+     
+        for (key, value) in httpBodyParameters.allValues() {
+            let values = ["--\(boundary)\r\n",
+                "Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n",
+                "\(value)\r\n"]
+     
+            _ = body.append(values: values)
+        }
+     
+        return body
+    }
+    private func add(files: [FileInfo], toBody body: inout Data, withBoundary boundary: String) -> [String]? {
+        var status = true
+        var failedFilenames: [String]?
+     
+        for file in files {
+            guard let filename = file.filename, let content = file.fileContents, let mimetype = file.mimetype, let name = file.name else { continue }
+            status = false
+            var data = Data()
+     
+            let formattedFileInfo = ["--\(boundary)\r\n",
+                "Content-Disposition: form-data; name=\"\(name)\"; filename=\"\(filename)\"\r\n",
+                "Content-Type: \(mimetype)\r\n\r\n"]
+     
+            if data.append(values: formattedFileInfo) {
+                if data.append(values: [content]) {
+                    if data.append(values: ["\r\n"]) {
+                        status = true
+                    }
+                }
+            }
+     
+            if status {
+                body.append(data)
+            } else {
+                if failedFilenames == nil {
+                    failedFilenames = [String]()
+                }
+     
+                failedFilenames?.append(filename)
+            }
+        }
+     
+        return failedFilenames
+    }
+    
+    
+    func upload(files: [FileInfo], toURL url: URL,
+                withHttpMethod httpMethod: HttpMethod,
+                completion: @escaping(_ result: Results, _ failedFiles: [String]?) -> Void) {
+     
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            let targetURL = self?.addURLQueryParameters(toURL: url)
+            guard let boundary = self?.createBoundary() else { completion(Results(withError: CustomError.failedToCreateBoundary), nil); return }
+            self?.requestHttpHeaders.add(value: "multipart/form-data; boundary=\(boundary)", forKey: "content-type")
+     
+            guard var body = self?.getHttpBody(withBoundary: boundary) else { completion(Results(withError: CustomError.failedToCreateHttpBody), nil); return }
+            let failedFilenames = self?.add(files: files, toBody: &body, withBoundary: boundary)
+            self?.close(body: &body, usingBoundary: boundary)
+     
+            guard let request = self?.prepareRequest(withURL: targetURL, httpBody: body, httpMethod: httpMethod) else { completion(Results(withError: CustomError.failedToCreateRequest), nil); return }
+     
+            let sessionConfiguration = URLSessionConfiguration.default
+            let session = URLSession(configuration: sessionConfiguration)
+            let task = session.uploadTask(with: request, from: nil, completionHandler: { (data, response, error) in
+                completion(Results(withData: data,
+                                   response: Response(fromURLResponse: response),
+                                   error: error),
+                           failedFilenames)
+            })
+            task.resume()
+        }
+     
+    }
+
+    
+    
+
+
 }
-
-
-
 // MARK: - Data Extension
 extension Data {
-    
+    mutating func append<T>(values: [T]) -> Bool {
+            var newData = Data()
+            var status = true
+     
+            if T.self == String.self {
+                for value in values {
+                    guard let convertedString = (value as! String).data(using: .utf8) else { status = false; break }
+                    newData.append(convertedString)
+                }
+            } else if T.self == Data.self {
+                for value in values {
+                    newData.append(value as! Data)
+                }
+            } else {
+                status = false
+            }
+     
+            if status {
+                self.append(newData)
+            }
+     
+            return status
+        }
 }
--- FileUploader/FileUploader/ViewController.swift
+++ FileUploader/FileUploader/ViewController.swift
@@ -14,9 +14,36 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        uploadSingleFile()
         // Do any additional setup after loading the view.
     }
-
+    func uploadSingleFile() {
+        let fileURL = Bundle.main.url(forResource: "sampleText", withExtension: "txt")
+        let fileInfo = RestManager.FileInfo(withFileURL: fileURL, filename: "sampleText.txt", name: "uploadedFile", mimetype: "text/plain")
+    }
+    func upload(files: [RestManager.FileInfo], toURL url: URL?) {
+        if let uploadURL = url {
+            rest.upload(files: files, toURL: uploadURL, withHttpMethod: .post) { (results, failedFilesList) in
+                print("HTTP status code:", results.response?.httpStatusCode ?? 0)
+     
+                if let error = results.error {
+                    print(error)
+                }
+     
+                if let data = results.data {
+                    if let toDictionary = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) {
+                        print(toDictionary)
+                    }
+                }
+     
+                if let failedFiles = failedFilesList {
+                    for file in failedFiles {
+                        print(file)
+                    }
+                }
+            }
+        }
+    }
 
 }
 
